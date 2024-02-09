f60b75c78ab2d7a9fd3d46e57619675c894f3165
Nastya N <asrozova@gmail.com>
2024-02-09 10:39:15 +0100

    fix: button content, gallery img, max-width

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,17 +1,18 @@
 import "./Button.scss";
 
 function Button({ text, icon, type }) {
-
   return (
     <button className={type}>
-      <span className={text ? "button__text" : "button__text_disable"}>
-        {text}
+      <span className="button__content">
+        <span className={text ? "button__content__text" : "button__content__text_disable"}>
+          {text}
+        </span>
+        <img
+          src={icon}
+          alt=""
+          className={icon ? "button__content__icon" : "button__content__icon_disable"}
+        />
       </span>
-      <img
-        src={icon}
-        alt=""
-        className={icon ? "button__icon" : "button__icon_disable"}
-      />
     </button>
   );
 }
