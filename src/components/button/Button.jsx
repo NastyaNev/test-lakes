5a6cfa5118d72df955cbb911db8561267db10116
Nastya N <asrozova@gmail.com>
2024-02-09 19:30:18 +0100

    feat: custom arrows gallery

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,16 +1,22 @@
 import "./Button.scss";
 
-function Button({ text, icon, type }) {
+function Button({ text, icon, type, className, onClick }) {
   return (
-    <button className={type}>
+    <button className={`${type} ${className}`} onClick={onClick}>
       <span className="button__content">
-        <span className={text ? "button__content__text" : "button__content__text_disable"}>
+        <span
+          className={
+            text ? "button__content__text" : "button__content__text_disable"
+          }
+        >
           {text}
         </span>
         <img
           src={icon}
           alt=""
-          className={icon ? "button__content__icon" : "button__content__icon_disable"}
+          className={
+            icon ? "button__content__icon" : "button__content__icon_disable"
+          }
         />
       </span>
     </button>
